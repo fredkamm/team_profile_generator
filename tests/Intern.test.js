9a31773e49c23961aef8ab5b97aac06c07bbeffd
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-13 18:41:57 -0400

    all 4 test pass sucessfully at the moment, next is to work on the prompt and the html generator

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,11 @@
 const Intern = require('../lib/intern');
+const intern = new Intern('fred', '1', 'fred@gmail.com', 'fred')
+
+describe("Intern", () => {
+    describe('name', () => {
+        it(`should return the name fred`, () => {
+            expect(intern.name).toEqual('fred')
+        })
+
+    })
+})
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,11 @@
 const Manager = require('../lib/manager');
+const manager = new Manager('fred', '1', 'fred@gmail.com', 'fred')
+
+describe("Manager", () => {
+    describe('name', () => {
+        it(`should return the name fred`, () => {
+            expect(manager.name).toEqual('fred')
+        })
+
+    })
+})
